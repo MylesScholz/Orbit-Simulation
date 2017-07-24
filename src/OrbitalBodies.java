6333a6f4580d7d9cfb55e2c62d0ee0bf63313d5e
Huy Nguyen <thanhloanhuynguyen@gmail.com>
2017-07-24 11:49:32 -0700

    new files

---
+++ src/OrbitalBodies.java
@@ -0,0 +1,24 @@
+
+public class OrbitalBodies {
+	
+	float mass;
+	
+	float xPosition;
+	float yPosition;
+	
+	float xVelocity;
+	float yVelocity;
+	
+	float xAcceleration;
+	float yAcceleration;
+	
+	
+	public void changePosition() {
+		
+	}
+
+	public void changeVelocity() {
+		
+	}
+	
+}
--- src/OrbitalPhysics.java
+++ src/OrbitalPhysics.java
@@ -1,5 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class OrbitalPhysics {
-
+	public static void main(String [] args)
+	{
+	
+	}
+	private float iterateVelocity(float initVelocity, float acceleration, float time) {
+		float velocity = initVelocity + 1/2 * acceleration * time;
+		return velocity;
+	}
+	
+	private float iterateAcceleration(HashMap body) {
+		
+		/*Cowell's Method*/
+		float acceleration = 0;
+		
+		
+		return acceleration;
+	}
+	
 	
 }
