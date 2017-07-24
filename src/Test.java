22104a620e026432bf7ae32eccc2522121ea1566
Matthew <mda31415@gmail.com>
2017-07-24 13:52:52 -0700

    Added graphics2d examples to use when needed

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,14 @@
 import java.util.Scanner;
+import java.awt.Graphics2D;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+
 
 public class Test {
 	public void Input() {
@@ -18,5 +28,16 @@ public class Test {
 		
 		System.out.println("Enter the object's mass:");
 		String mass = scanner.nextLine();
+		
 	}
 }
+/*
+public void paint(Graphics g){
+	Graphics2D solarSystem = (Graphics2D)g;
+	solarSystem.setPaint(Color.BLACK); 
+	Shape drawLine = new Line2D.Float(20, 90, 55, 250);
+	Shape drawOval = new Ellipse2D.Float(120, 190, 155, 350)
+	solarSystem.draw(drawLine);
+	solarSystem.draw(drawEllipse);
+}
+*/
