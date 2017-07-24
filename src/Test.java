47aeb009f95f010c676f798944f3c02e7cdb417b
Matthew <mda31415@gmail.com>
2017-07-24 12:59:52 -0700

    Added inputs for position, vector, and mass

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Test {
-
+	public void Input() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the object's x position:");
+		String xPosition = scanner.nextLine();
+		
+		System.out.println("Enter the object's y position:");
+		String yPosition = scanner.nextLine();
+		
+		System.out.println("Enter the object's x vector:");
+		String xVector = scanner.nextLine();
+		
+		System.out.println("Enter the object's y vector:");
+		String yVector = scanner.nextLine();
+		
+		System.out.println("Enter the object's mass:");
+		String mass = scanner.nextLine();
+	}
 }
