63849df6b8b07ac4eacb28d3476b6ad4e61ea724
Huy Nguyen <thanhloanhuynguyen@gmail.com>
2017-07-26 14:37:43 -0700

    test: desktop module only

--- README.md
+++
@@ -1,2 +0,0 @@
-# Orbit-Simulation
-The GitHub repository for the orbital mechanics simulation.
---
+++ desktop/src/com/nwapw/orbitmechanics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nwapw.orbitmechanics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nwapw.orbitmechanics.RunSimulation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunSimulation(), config);
+	}
+}
